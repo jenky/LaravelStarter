cf4abbafc0b7a36d6cbe92a44f099425c4ac1260
Milano <jenky.w0w@gmail.com>
2015-03-12 11:43:38 +0700

    Codeception

---
+++ tests/functional/RoutesCest.php
@@ -0,0 +1,27 @@
+<?php
+
+class RoutesCest
+{
+
+    public function openPageByRoute(FunctionalTester $I)
+    {
+        $I->amOnRoute('posts.index');
+        $I->seeCurrentUrlEquals('/posts');
+        $I->seeCurrentActionIs('PostsController@index');
+    }
+
+    public function openPageByAction(FunctionalTester $I)
+    {
+        $I->amOnAction('PostsController@index');
+        $I->seeCurrentUrlEquals('/posts');
+        $I->seeCurrentRouteIs('posts.index');
+    }
+
+    public function openRouteWithDomainSpecified(FunctionalTester $I)
+    {
+        $I->amOnRoute('domain');
+        $I->seeResponseCodeIs(200);
+        $I->see('Domain route');
+    }
+
+}
