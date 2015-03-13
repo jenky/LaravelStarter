e0bb28677d02f57aed02a80e5fb004a2e72ccbbe
Milano <jenky.w0w@gmail.com>
2015-03-13 12:01:39 +0700

    Clear error list before response

--- app/Helpers/Api.php
+++ app/Helpers/Api.php
@@ -4,35 +4,78 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Api extends Http {
 
+	/**
+	 * List of custom error codes and messages
+	 * 
+	 * @var array
+	 */ 
 	public static $customStatusTexts = [
 		// 1000 => 'Missing required field'
 	];
 
-	protected static $errors;
+	/**
+	 * List Erros
+	 * 
+	 * @var array
+	 */ 
+	protected static $errors = [];
 
+	/**
+	 * Respone json errors payload
+	 * 
+	 * @param int $code
+	 * @param string $message
+	 * @param int $status
+	 * 
+	 * @return \Response
+	 */ 
 	public function responseError($code = null, $message = '', $status = 400)
     {
         return $this->error($code, $message)->response($status);
     }
 
+    /**
+	 * Respone json errors payload
+	 * 
+	 * @param array $errors
+	 * @param string $message
+	 * @param int $status
+	 * 
+	 * @return \Response
+	 */ 
     public function responseErrors(array $errors, $status = 400)
     {
     	return $this->errors($errors)->response($status);
     }
 
-    public function getErrorMessage($code = null, $message = '')
+    /**
+	 * Get error message based on http error codes and custom error codes
+	 * 
+	 * @param int $code
+	 * @param string $message
+	 * 
+	 * @return array
+	 */ 
+    public function getErrorMessage($code, $message = '')
     {
     	$statusTexts = static::$customStatusTexts + Response::$statusTexts;
 
-        $_code = $code ? $code : $status;
-        $message = (isset($statusTexts[$_code]) && !$message) ? $statusTexts[$_code] : $message;
+        $message = (isset($statusTexts[$code]) && !$message) ? $statusTexts[$code] : $message;
 
         return [
-            'code' => $_code,
+            'code' => $code,
             'message' => $message
         ];
     }
 
+    /**
+     * Set error code and message
+     * 
+     * @param int $code
+     * @param string $message
+     * 
+     * @return App\Helpers\Api
+     */ 
     public function error($code, $message = '')
     {
 		static::$errors[] = $this->getErrorMessage($code, $message);
@@ -40,6 +83,13 @@ class Api extends Http {
 		return $this;
     }
 
+    /**
+     * Set error codes and messages
+     * 
+     * @param array $errors
+     * 
+     * @return App\Helpers\Api
+     */ 
     public function errors($errors)
     {
 		static::$errors = (array) $errors;
@@ -47,10 +97,20 @@ class Api extends Http {
 		return $this;
     }
 
+    /**
+	 * Respone json errors payload
+	 * 
+	 * @param int $status
+	 * 
+	 * @return \Response
+	 */ 
     public function response($status = 400)
     {
+		$errors = static::$errors;
+		static::$errors = [];
+
 		return \Response::json([
-		    'errors' => static::$errors
+		    'errors' => $errors
 		], $status);
     }
 }
