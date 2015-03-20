36ca22e469f616aeb6ff672d6201aea69951ba11
Milano <jenky.w0w@gmail.com>
2015-03-20 15:13:35 +0700

    Indent with tab

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>    
-    <title>{{{ $title or 'Email' }}}</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>    
+	<title>{{{ $title or 'Email' }}}</title>
 </head>
 <body>
 <table cellpadding="0" cellspacing="0" border="0" width="100%">
-    {{ $content }}
+	{{ $content }}
 </table>
 </body>
 </html>
