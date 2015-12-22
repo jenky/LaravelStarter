13af1e429ced4e8496f39582a54d8fb3f403867f
Lynh <milano1411@gmail.com>
2015-12-22 10:03:07 +0700

    Minor changes

--- app/Contracts/Repositories/UserRepository.php
+++ app/Contracts/Repositories/UserRepository.php
@@ -10,8 +10,16 @@ interface UserRepository
      * Create a new user.
      * 
      * @param  \Illuminate\Http\Request  $request
-     * 
      * @return \Illuminate\Contracts\Auth\Authenticatable
      */
     public function create(Request $request);
+
+    /**
+     * Update an user.
+     * 
+     * @param  int $id
+     * @param  array $data
+     * @return \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public function update($id, array $data);
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -7,22 +7,51 @@ use Illuminate\Http\Request;
 
 class UserRepository implements Contract
 {
+    /** 
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected $model;
+
+    /**
+     * Class contructor.
+     */
+    public function __construct()
+    {
+        $model = config('auth.providers.users.model');
+        $this->model = new $model;
+    }
+
     /**
      * Create a new user.
      * 
      * @param  \Illuminate\Http\Request  $request
-     * 
      * @return \Illuminate\Contracts\Auth\Authenticatable
      */
     public function create(Request $request)
     {
-        $model = config('auth.model');
-
-        return (new $model)->create([
+        return $this->model->create([
             'first_name' => $request->input('first_name'),
             'last_name'  => $request->input('last_name'),
             'email'      => $request->input('email'),
             'password'   => bcrypt($request->input('password')),
         ]);
+    }
+
+    /**
+     * Update an user.
+     * 
+     * @param  int $id
+     * @param  array $data
+     * @return \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public function update($id, array $data)
+    {
+        if (isset($data['password'])) {
+            $data['password'] = bcrypt($data['password']);
+        }
+
+        $user = $this->model->findOrFail($id);
+
+        return $user->update($data);
     }
 }
