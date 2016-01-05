5c2dfa70d70ee4aee9d07eb51f760b84789fa1d0
Lynh <milano1411@gmail.com>
2016-01-05 11:47:22 +0700

    Remove blade provider

--- config/app.php
+++ config/app.php
@@ -164,7 +164,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\HelperServiceProvider::class,
-        App\Providers\BladeServiceProvider::class,
     ],
 
     /*
