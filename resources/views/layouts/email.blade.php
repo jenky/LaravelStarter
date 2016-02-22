21de0e4793ee55b937818135f9c076cc74516e53
Lynh <milano1411@gmail.com>
2016-02-22 15:23:58 +0700

    Update email layout

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>    
-    <title>{{{ $title or 'Email' }}}</title>
+    <meta http-equiv="Content-Type" content="text/html; charset={{ $charset or 'utf-8' }}"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <title>{{ $title or config('app.title') }}</title>
 </head>
 <body>
 <table cellpadding="0" cellspacing="0" border="0" width="100%">
-    {!! $content !!}
+    @yield('content')
 </table>
 </body>
 </html>
