db2beab6c46ee51699df65d09d0fc40ef0777503
Lynh <milano1411@gmail.com>
2016-03-22 16:39:14 +0700

    Add language files

---
+++ resources/lang/en/error.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'whoops'     => 'Whoops! Something went wrong. Please try again later.',
+    'try_again'  => 'Please try again later.',
+    'oauth_auth' => 'We can not retrieve your credentials via a :provider',
+
+];
