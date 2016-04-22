b697cf409b36c90315a050e9f7ac30df67123999
Lynh <milano1411@gmail.com>
2016-04-22 15:57:00 +0700

    Update error language

--- resources/lang/en/error.php
+++ resources/lang/en/error.php
@@ -1,8 +1,18 @@
 <?php
 
 return [
-    'whoops'     => 'Whoops! Something went wrong. Please try again later.',
-    'try_again'  => 'Please try again later.',
-    'oauth_auth' => 'We can not retrieve your credentials via a :provider',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Error Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for error message.
+    |
+    */
+
+    'not_found' => 'The requested :resource could not be found.',
+    'whoops' => 'Whoops! Something went wrong. Please try again later.',
+    'try_again' => 'Please try again later.',
+    'no_permission' => 'You do not have permission to perform this action.',
 ];
