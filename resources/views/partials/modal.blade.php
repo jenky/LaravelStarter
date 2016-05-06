b622a1bb0ed9df15b5347b682a1810dbf20e318b
Lynh <milano1411@gmail.com>
2016-05-06 15:08:49 +0700

    Add modal template

---
+++ resources/views/partials/modal.blade.php
@@ -0,0 +1,23 @@
+<div class="{{ $modalClass or 'modal fade' }}" role="dialog" tabindex="-1">
+    <div class="modal-dialog {{ $modalSize or '' }}">
+        <div class="modal-content">
+            @section('modal-content')
+                <div class="modal-header">
+                    @if (!empty($modalClose))
+                        <button aria-label="{{ trans('ui.close') }}" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+                    @endif
+                    <h4 class="modal-title">{{ $modalTitle or '' }}</h4>
+                </div>
+                <div class="modal-body">
+                    @yield('modal-body')
+                </div>
+                <div class="modal-footer">
+                    @if (!empty($modalClose))
+                        <button class="btn {{ $modalCloseBtn or 'btn-default' }}" data-dismiss="modal" type="button">{{ trans('ui.close') }}</button>
+                    @endif
+                    @yield('modal-footer')
+                </div>
+            @show
+        </div>
+    </div>
+</div>
