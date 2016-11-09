28ede394111a141f9a9e140d156c487921a7fbaf
Lynh Tran <jenky.w0w@gmail.com>
2016-11-09 09:13:34 +0700

    Sync with original
    laravel/laravel@cddb8a2
    laravel/laravel@e72463d

--- config/mail.php
+++ config/mail.php
@@ -55,8 +55,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
