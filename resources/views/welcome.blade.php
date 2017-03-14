468d7f8cc96ee51a290391487509ac036b1495bc
Lynh <jenky.w0w@gmail.com>
2017-03-14 10:13:04 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.4
+                    Laravel
                 </div>
 
                 <div class="links">
