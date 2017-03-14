7d2ca84d88fb9a083dd10b719a719ff3b2a290d7
Phai Nguyen <nguyenphai.cntt@gmail.com>
2017-03-14 10:10:05 +0700

    Laravel 5.4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 5.4
                 </div>
 
                 <div class="links">
