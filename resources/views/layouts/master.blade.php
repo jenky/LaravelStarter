b25b9267d4a2e64d1276a617dfc9596137a888a7
Lynh Tran <jenky.w0w@gmail.com>
2018-02-06 10:56:20 +0700

    Update master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="@yield('htmlLang', app()->getLocale())" class="@yield('htmlClass')">
 <head>
     @section('head')
     <meta charset="@yield('charset', 'utf-8')">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @yield('meta')
@@ -21,7 +21,7 @@
     @show
 </head>
 <body class="@yield('bodyClass')">
-@yield('body')
+    @yield('body')
+    @yield('js')
 </body>
-@yield('js')
 </html>
