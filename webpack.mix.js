14ab325612ed7052e2874880ffbec3d7d995e69c
Lynh Tran <jenky.w0w@gmail.com>
2019-01-23 23:44:35 +0700

    Extract vendor automatically

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix')
  */
 
 mix.js('resources/js/app.js', 'public/js')
-  .extract(['jquery', 'bootstrap', 'vue', 'axios'])
+  .extract()
   .autoload({
     jquery: ['$', 'window.jQuery', 'jQuery', 'jquery'],
   })
