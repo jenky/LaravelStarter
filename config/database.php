3b89d8923f01d8e24b141af2070b51f38c5cb8bb
Lynh Tran <jenky.w0w@gmail.com>
2019-07-19 15:04:11 +0700

    Sync with original

--- config/database.php
+++ config/database.php
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
+            env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
